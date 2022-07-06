807a60b12d1d442ba4c20c31d9a53e434061db7b
Tiago <thenrique395@gmail.com>
2022-07-05 21:21:54 -0300

    First commit

---
+++ DesignerPatternsCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignerPatternsCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
